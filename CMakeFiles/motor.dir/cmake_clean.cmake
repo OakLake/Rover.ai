d373bec36744293aefff198cdd76e639f95e308f
oaklake <pi@raspberrypi.sam>
2018-05-20 17:45:25 +0000

    cmake edition 9001

---
+++ CMakeFiles/motor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motor.dir/src/motor_test.cpp.o"
+  "CMakeFiles/motor.dir/src/motorlib.cpp.o"
+  "motor.pdb"
+  "motor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
